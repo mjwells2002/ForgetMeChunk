b2dd3bc1aeb60961c64237ded195cb75455a7d63
mjwells <36360408+mjwells2002@users.noreply.github.com>
2022-02-27 15:28:01 +0000

    First Version

---
+++ src/main/java/xyz/breadloaf/forgetmechunk/mixin/ClientPacketListenerMixin.java
@@ -0,0 +1,16 @@
+package xyz.breadloaf.forgetmechunk.mixin;
+
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.core.SectionPos;
+import net.minecraft.world.level.lighting.LevelLightEngine;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ClientPacketListener.class)
+public class ClientPacketListenerMixin {
+    @Redirect(method="handleForgetLevelChunk", at=@At(value = "INVOKE", target = "Lnet/minecraft/world/level/lighting/LevelLightEngine;updateSectionStatus(Lnet/minecraft/core/SectionPos;Z)V"))
+    private void injected(LevelLightEngine instance, SectionPos sectionPos, boolean bl) {
+
+    }
+}
