05c3a9a6aab0c4a59366ef2c03f3cbf85701e168
mjwells <36360408+mjwells2002@users.noreply.github.com>
2022-02-27 17:16:52 +0000

    it worked in dev i swear

--- src/main/java/xyz/breadloaf/forgetmechunk/mixin/ClientPacketListenerMixin.java
+++ src/main/java/xyz/breadloaf/forgetmechunk/mixin/ClientPacketListenerMixin.java
@@ -1,16 +1,32 @@
 package xyz.breadloaf.forgetmechunk.mixin;
 
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.core.SectionPos;
+import net.minecraft.network.protocol.game.ClientboundForgetLevelChunkPacket;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.lighting.LevelLightEngine;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPacketListener.class)
 public class ClientPacketListenerMixin {
-    @Redirect(method="lambda$queueLightUpdate$3", at=@At(value = "INVOKE", target = "Lnet/minecraft/world/level/lighting/LevelLightEngine;updateSectionStatus(Lnet/minecraft/core/SectionPos;Z)V"))
-    private void injected(LevelLightEngine instance, SectionPos sectionPos, boolean bl) {
+    @Shadow private ClientLevel level;
 
+    @Inject(method = "queueLightUpdate(Lnet/minecraft/network/protocol/game/ClientboundForgetLevelChunkPacket;)V", at = @At("HEAD"), cancellable = true)
+    private void injected(ClientboundForgetLevelChunkPacket clientboundForgetLevelChunkPacket, CallbackInfo ci) {
+        this.level.queueLightUpdate(() -> {
+            LevelLightEngine levelLightEngine = this.level.getLightEngine();
+            for (int i = this.level.getMinSection(); i < this.level.getMaxSection(); ++i) {
+                this.level.setSectionDirtyWithNeighbors(clientboundForgetLevelChunkPacket.getX(), i, clientboundForgetLevelChunkPacket.getZ());
+            }
+            levelLightEngine.enableLightSources(new ChunkPos(clientboundForgetLevelChunkPacket.getX(), clientboundForgetLevelChunkPacket.getZ()), false);
+            this.level.setLightReady(clientboundForgetLevelChunkPacket.getX(), clientboundForgetLevelChunkPacket.getZ());
+        });
+        ci.cancel();
     }
 }
